ed21b52e46316b0aaeb6a3ff0b8b8d65b02104d7
jiangliulin <jiangliulin@163.com>
2019-05-14 15:27:17 +0800

    settings优化

--- venv/config/settings.py
+++ venv/config/settings.py
@@ -3,13 +3,6 @@ CASE_NAME = "case_name"
 SQL_EXP = "sql_exp"
 RESULT = "result"
 
-EXPLAIN = "explain {}"
-TEST_CASE_SQL = "select * from `sql_cases`;"
-CLEAR_RESULT_SQL = "update `sql_cases` set result='';"
-UPDATE_RESULT_SQL = "update sql_cases set result=%s where case_id=%s;"
-RESULT_SQL = "select {},{},{} from `sql_cases`;".format(CASE_ID,SQL_EXP,RESULT)
-RESULT_TYPE = "type"
-
 DB_TYPE = "mysql"
 DB_HOST = "127.0.0.1"
 DB_USER = "root"
@@ -18,12 +11,19 @@ DB_PASSWD = "122901"
 DB_NAME = "testing"
 TABLE_NAME = "`sql_cases`"
 
+EXPLAIN = "explain {}"
+TEST_CASE_SQL = "select * from {};".format(TABLE_NAME)
+CLEAR_RESULT_SQL = "update {} set result='';".format(TABLE_NAME)
+UPDATE_RESULT_SQL = "update {} set {}=%s where {}=%s;".format(TABLE_NAME,RESULT,CASE_ID)
+RESULT_SQL = "select {},{},{} from {};".format(CASE_ID,SQL_EXP,RESULT,TABLE_NAME)
+RESULT_TYPE = "type"
+
 BEAUTIFUL_REPORT_CONTENT = "case_id: {}\n sql语句: {}\n 执行结果: {}"
 
 EMAIL_CONTENT = "本次sql 执行计划执行了{}个测试用例，其中运行成功{}个，运行失败{}个。"\
-        "其中type为ALL的sql有{}个,为{},其中type为INDEX的sql有{}个,为{}。\n"\
+        "其中type为ALL的sql有{}个,为{}\n其中type为INDEX的sql有{}个,为{}。\n"\
         "测试报告地址: {}\n"\
-        "可在数据库中查看运行结果，数据库类型为{}，数据库地址:{}，数据库用户名:{}，密码:{}，数据库库名:{}，表名:{}"
+        "可在数据库中查看运行结果，数据库类型:{}，数据库地址:{}，数据库用户名:{}，密码:{}，数据库库名:{}，表名:{}"
 
 
 EMAIL_SUB = "sql 执行计划测试报告   {}"
